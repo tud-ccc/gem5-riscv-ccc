d8779a868a51f4d5f808514e5e42d61b82ee32d8
Robert Scheffel <robert.scheffel@commsolid.com>
2018-07-12 11:09:16 +0200

    configuration for cld samara board
    Change-Id: Ifb9a736658c268c1c0df37f4d673f93e511f667c

---
+++ configs/example/riscv/fs_samara.py
@@ -0,0 +1,100 @@
+# Copyright (c) 2018 TU Dresden
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met: redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer;
+# redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution;
+# neither the name of the copyright holders nor the names of its
+# contributors may be used to endorse or promote products derived from
+# this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# Authors: Robert Scheffel
+
+'''
+Full system script
+'''
+
+import argparse
+from samara import Samara
+
+import m5
+from m5.objects import *
+
+m5.util.addToPath('../..')
+from common import MemConfig
+
+cpu_types = {
+    'atomic': AtomicSimpleCPU,
+    'timing': TimingSimpleCPU,
+    'minor': MinorCPU
+}
+
+
+def create(args):
+    '''Create the system and configure it'''
+    cpu_class = cpu_types[args.cpu]
+    mem_mode = cpu_class.memory_mode()
+
+    system = Samara(cpu_class=cpu_class,
+                    wfgdb=args.wait_for_gdb,
+                    mem_mode=mem_mode,
+                    bootloader=args.binary)
+
+    system.connect()
+
+    return system
+
+
+def run():
+    '''Run the simulation'''
+    exit_event = m5.simulate(m5.MaxTick)
+    print('Exiting because %s @ %d ' % (exit_event.getCause(), m5.curTick()))
+
+
+def main():
+    parser = argparse.ArgumentParser(epilog=__doc__)
+
+    parser.add_argument('-b',
+                        '--binary',
+                        type=str,
+                        default=None,
+                        required=True,
+                        help='The binary to run')
+    parser.add_argument('--cpu',
+                        type=str,
+                        default='minor',
+                        help='CPU model to use')
+    parser.add_argument('-w',
+                        '--wait-for-gdb',
+                        action='store_true',
+                        help='Wait for remote gdb connection '
+                        'before starting simulation')
+
+    args = parser.parse_args()
+
+    root = Root(full_system=True)
+    root.system = create(args)
+
+    m5.instantiate()
+
+    run()
+
+
+if __name__ == '__m5_main__':
+    main()
---
+++ configs/example/riscv/samara.py
@@ -0,0 +1,142 @@
+# Copyright (c) 2018 TU Dresden
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met: redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer;
+# redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution;
+# neither the name of the copyright holders nor the names of its
+# contributors may be used to endorse or promote products derived from
+# this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# Authors: Robert Scheffel
+
+'''
+System definition.
+'''
+
+from m5.objects import *
+from minor_custom_fu import MinorCustomFUPool
+
+
+class SamaraIntFU(MinorFU):
+    opClasses = minorMakeOpClassSet(['IntAlu'])
+    opLat = 5
+    issueLat = 5
+
+
+class SamaraIntMulFU(MinorFU):
+    opClasses = minorMakeOpClassSet(['IntMult'])
+    timings = [MinorFUTiming(description='Mul',
+                             srcRegsRelativeLats=[0])]
+    opLat = 6
+    issueLat = 6
+
+
+class SamaratIntDivFU(MinorFU):
+    opClasses = minorMakeOpClassSet(['IntDiv'])
+    issueLat = 40
+    opLat = 40
+
+
+class SamaraFUPool(MinorFUPool):
+    funcUnits = [SamaraIntFU(),
+                 SamaraIntMulFU(),
+                 SamaratIntDivFU(),
+                 MinorDefaultFloatSimdFU(),
+                 MinorDefaultMemFU(),
+                 MinorDefaultMiscFU()]
+
+
+class MemBus(SystemXBar):
+    badaddr_responder = BadAddr(warn_access="warn")
+    default = Self.badaddr_responder.pio
+
+    frontend_latency = 2
+    forward_latency = 1
+    response_latency = 2
+    snoop_response_latency = 1
+    snoop_filter = SnoopFilter(lookup_latency=1)
+
+
+class TimingMemBus(SystemXBar):
+    badaddr_responder = BadAddr(warn_access="warn")
+    default = Self.badaddr_responder.pio
+
+    frontend_latency = 0
+    forward_latency = 0
+    response_latency = 0
+    snoop_response_latency = 0
+    snoop_filter = SnoopFilter(lookup_latency=1)
+
+
+class Samara(BareMetalRiscvSystem):
+    """
+    Simple system containing just CPU, bus and memory
+    """
+
+    def __init__(self, cpu_class, wfgdb, **kwargs):
+        super(Samara, self).__init__(**kwargs)
+
+        # create clock and voltage domain
+        # set to 100MHz clock frequency (like real hw board)
+        self.clk_domain = SrcClockDomain(clock='100MHz')
+        self.clk_domain.voltage_domain = VoltageDomain()
+
+        # set up our board
+        self.board = SimpleBoard()
+        # appearently we need a dedicated interrupt controller
+        self.intrctrl = IntrControl()
+
+        # specify functional unit
+        self.execFU = SamaraFUPool()
+
+        # create cpu and thread
+        # the interruptcontroller needs to be created as well
+        # tell the cpu if it shall wait until a gdb gets
+        # connected remotely or not
+        self.cpu = cpu_class()
+        self.cpu.createThreads()
+        self.cpu.createInterruptController()
+        self.cpu.wait_for_remote_gdb = wfgdb
+
+        # system memory bus
+        self.membus = TimingMemBus()
+
+        # create mem_range
+        # for now start at 0x0
+        # take 3GB as size as it is a bit lower than 0xffffffff bytes
+        mem_start = Addr(0x00000000)
+        mem_size = '2GB'
+        self.mem_ranges = [AddrRange(start=mem_start, size=mem_size)]
+
+        self.ram = SimpleMemory(latency='12ns')
+        self.ram.range = self.mem_ranges[0]
+
+        # define bit mode
+        # self.rv32 = True
+
+    def connect(self):
+        # connect cache ports of cpu to membus
+        # no caches -> connect directly to mem bus
+        self.cpu.connectAllPorts(self.membus)
+        self.system_port = self.membus.slave
+
+        self.ram.port = self.membus.master
+
+        self.board.attachIO(self.membus)
